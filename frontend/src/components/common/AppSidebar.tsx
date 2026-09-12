c249093d2d8d90c5800587c0ade776d6de5c4c5d
wasim <wasimmiddya21@gmail.com>
2026-09-12 18:45:27 +0530

    Add 'cn' package and implement Table component with header, body, footer, and cell structures

--- frontend/src/components/common/AppSidebar.tsx
+++ frontend/src/components/common/AppSidebar.tsx
@@ -5,24 +5,34 @@ import {
     CollapsibleContent,
     CollapsibleTrigger,
 } from "@/components/ui/collapsible"
+import { Link } from "react-router-dom";
 
 export const AppSidebar: React.FC = () => {
+    const menuItems = [
+        { id: 1, name: 'Policy Management', uri: '/admin/permission' },
+        { id: 2, name: 'User Management', uri: '/admin/user' }
+    ]
     return (
         <Sidebar>
             <SidebarHeader className="mt-17">
-                <span className="font-bold text-gray-400">APP EXPLORER</span>
+                {/* <span className="font-bold text-gray-400">APP EXPLORER</span> */}
                 <Collapsible defaultOpen className="group/collapsible">
                     <SidebarGroup>
-                        <SidebarGroupLabel render={<CollapsibleTrigger />}>
-                            Help
+                        <SidebarGroupLabel className="text-lg" render={<CollapsibleTrigger />}>
+                            Admin Explorer
                             <ChevronDown className="ml-auto transition-transform group-data-open/collapsible:rotate-180" />
                         </SidebarGroupLabel>
-                        <CollapsibleContent>
-                            <SidebarGroupContent >
+                        <CollapsibleContent className="overflow-hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
+                            <SidebarGroupContent className="mt-1 rounded-lg border border-sidebar-border/60 bg-sidebar-accent/30 p-1">
                                 <SidebarMenu>
-                                    {Array.from({ length: 5 }).map((_, index) => (
-                                        <SidebarMenuItem key={index}>
-                                            <li>item-{index}</li>
+                                    {menuItems.map((mnu, _) => (
+                                        <SidebarMenuItem key={mnu.id}>
+                                            <Link
+                                                to={mnu.uri}
+                                                className="flex w-full items-center rounded-md px-3 py-2 text-sm font-medium text-sidebar-foreground/75 transition-colors hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sidebar-ring"
+                                            >
+                                                {mnu.name}
+                                            </Link>
                                         </SidebarMenuItem>
                                     ))}
                                 </SidebarMenu>
